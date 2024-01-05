fcdeaae3feea3005a5dac75bac2e7465b000ee0d
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-04 18:42:53 -0800

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import fast_alphabet
+import string
+print(string.ascii_uppercase)
--- 0x02-python-import_modules/fast_alphabet.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-for i in range(ord('a'), ord('z')):
-    print("{}".format(chr(i).upper()), end="")
-print()
