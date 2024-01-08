2ca6f2fe6147de42329e8b04948d9e9104078e7f
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-08 14:27:05 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    return None if (idx < 0 or idx >= len(my_list)) else my_list[idx]
