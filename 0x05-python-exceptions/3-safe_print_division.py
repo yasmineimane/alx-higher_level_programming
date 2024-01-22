6d12213158777921bb2c5fd13c454ef1b31f95ec
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-22 09:51:48 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     x = 0
     try:
         x = a / b
-    except:
+    except ZeroDivisionError:
         x = None
     finally:
         print("Inside result: {}".format(x))
