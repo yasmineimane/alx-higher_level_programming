2bebb3d936ed1134e98ac48602ce96c5d7b0bfc5
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-29 15:54:29 -0800

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: Length of a side of a square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the length of a side of this square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """Area of this square.
+    Returns:
+        The size squared.
+    """
+    def area(self):
+        return self.__size**2
+
+    """prints in stdout the square with the character #."""
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#"*self.__size, end='\n')
