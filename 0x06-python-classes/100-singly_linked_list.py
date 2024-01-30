5a3e82a2083bd1cac4234952622b6ec1325bd6d0
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-30 12:01:02 -0800

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -36,6 +36,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """Represents a singly linked list"""
 
