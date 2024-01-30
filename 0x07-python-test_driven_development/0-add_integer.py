88b5001105fcc882a214868741063e5350fe380b
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-01-30 12:51:05 -0800

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for add_integer method."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: the first integer.
+        b: the second integer, default 98.
+
+    Raises:
+        TypeError: if a, b are not int, float.
+
+    Return:
+        the sum of the two integers.
+    """
+
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
