d0e6338dcf2b2a3d327c31ff24ba054d0b69c62f
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-02 05:54:17 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,8 +15,6 @@ def add_integer(a, b=98):
     Return:
         the sum of the two integers.
     """
-
-
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
