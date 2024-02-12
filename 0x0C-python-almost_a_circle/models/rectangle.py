bb22ff709e9205e56ddb4c8b9d8b889986ffd569
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-12 13:14:00 -0800

    Rctangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,5 +106,4 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '''Returns dictionary representation of this class.'''
-        return {"id": self.id, "width": self.__width, "height": self.__height,
-                "x": self.__x, "y": self.__y}
+        return {"id": self.id, "width": self.width, "height": self.height, "x": self.x, "y": self.y}
