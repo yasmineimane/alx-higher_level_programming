92e3f2859bb35d6118faea3c8037793267c67368
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-12 08:40:04 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for Base class.
+"""
+
+
+class Base:
+    """
+    Defines Base class.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
