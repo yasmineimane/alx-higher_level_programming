320660a04fb29fe697eda8033579a2865e60ee87
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-12 18:00:14 -0700

    Count 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
 -- script that displays the number of records with id = 89 in the table first_table
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(id) 
+FROM first_table
+WHERE id = 89;
