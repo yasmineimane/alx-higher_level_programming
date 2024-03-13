5b2ff594e661b8f0eed0a515154de4f7de9c146d
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-12 17:49:50 -0700

    List all in table

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- script that lists all rows of the table first_table from the database hbtn_0c_0
-select * from first_table;
+-- script that lists all rows of the table first_table
+SELECT * FROM first_table;
