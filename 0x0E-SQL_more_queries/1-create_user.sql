fc73d07be0550c4d56d194a138ffd2acd8e55f1a
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-15 23:03:38 +0100

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- script that creates the MySQL server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
