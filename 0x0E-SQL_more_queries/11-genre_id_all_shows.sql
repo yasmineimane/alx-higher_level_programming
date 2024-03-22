a0bae3df74ec27d6dd64844af6b6486fb57e623d
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-22 09:04:46 -0700

    Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- script that lists all shows contained in the database hbtn_0d_tvshows
+-- Script that lists all shows contained in a database
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.tv_show_id
-ORDER BY tv_shows.title,
-tv_show_genres.genre_id;
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
