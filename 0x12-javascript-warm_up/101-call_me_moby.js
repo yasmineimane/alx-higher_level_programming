3321a449fb7e82a051764e5ea5cca8cf6a19f5c6
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-24 09:40:14 -0700

    Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction)
-{
-	for (let i = 0; i < x; i++)
-	{
-		theFunction();
-	}
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
