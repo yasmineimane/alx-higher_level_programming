64c0c1425309c27a20d6eb987f90f978677b985c
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-24 10:29:43 -0700

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+constructor(w, h)
+{
+if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h))
+{
+return {};
+}
+this.width = w;
+this.height = h;
+}
+module.exports = Rectangle;
