5f6170e5f6d6239cf470eb819084c158c7087dec
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-24 10:37:49 -0700

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,12 @@
 class Rectangle {
 constructor(w, h)
 {
-if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h))
-{
-return Rectangle {};
-}
-this.width = w;
-this.height = h;
+if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
 }
 }
 module.exports = Rectangle;
