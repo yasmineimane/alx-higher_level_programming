167155ee870825a39bf8c92eaacb1a0bd1f043c7
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-25 09:26:07 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+class Square2 extends Square {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      console.log('C'.repeat(this.size));
+    }
+  }
+}
+module.exports = Square2;
