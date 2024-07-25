d3489780aafef8bdb39114ca968782709eae557a
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-25 10:11:19 -0700

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const list2 = [];
-const length = list.length;
-  for (let i = length -1; i >= 0; i--) {
-    for (let j = 0; j < length; j++) {
-	list2[j] = list[i];
+  const length = list.length;
+  for (let i = length - 1; i >= 0; i--) {
+    list2.push(list[i]);
   }
   return list2;
 };
