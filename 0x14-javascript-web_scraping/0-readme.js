b72e76efbd3dc287b04b2870500d6ea063473cbd
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-30 06:24:32 -0700

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
