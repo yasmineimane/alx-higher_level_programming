b467ca4f1215a29a5e511dcceafebb2c7d332320
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-07-30 07:12:35 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,9 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (error, response) => {
-  if (response) {
-    console.log('Code:', response.statusCode);
+  if (error) {
+    console.error('Error:', error.message);
+  } else {
+    console.log('code:', response.statusCode);
   }
 });
